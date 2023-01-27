8a46e2fcde655b397d4064d77106cbf021cf92ca
Yhudem10 <darkpendragom@gmail.com>
2023-01-27 15:56:45 -0300

    Teste

---
+++ Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaStringB.Entities
+{
+    internal class Comment
+    {
+        public String Text { get; set; }
+        public Comment()
+        {
+
+        }
+        public Comment(string text)
+        {
+            Text= text; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using AulaStringB.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that´s awesome!");
+
+        Post p1 = new Post(
+            DateTime.Parse("21/05/2018 13:05:44"),
+            "Travaling a New Zealand",
+            "I´m going to visit this wonderful country",
+            12
+             ) ;
+        p1.AddComment(c1);
+        p1.AddComment(c2);
+
+        Console.WriteLine(p1);
+    }
+}
